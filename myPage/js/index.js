aa9aa2b8fa5777319ffc599b1bc4cef7c3d505b4
sunhuina <634241604@qq.com>
2017-11-18 18:25:03 +0800

    添加上拉加载下拉刷新

--- myPage/js/index.js
+++ myPage/js/index.js
@@ -16,16 +16,16 @@ $(function(){
 
       Vue.component('todo-item', {
         props: ['todo'],
-        template: '<div class="bar_group__bar thin" style="width:{{todo.rate}}"></div><a href="/view/{{todo.href}}" class="skill-name">{{ todo.text }}</a>'
+        template: '<div class="bar_group__bar thin" v-bind:style="todo.style"></div><a href="/view/{{todo.href}}" class="skill-name">{{ todo.text }}</a>'
       })
       var app7 = new Vue({
         el: '#app-7',
         data: {
           groceryList: [
-            { id: 0, text: 'html5,css3' ,rate:"90%",href:"htmlcss"},
-            { id: 1, text: 'javascript' ,rate:"80%",href:"javascript"},
-            { id: 2, text: 'vue.js' ,rate:"70%",href:"vue"},
-            { id: 3, text: 'webpack' ,rate:"80%",href:"webpack"}
+            { id: 0, text: 'html5,css3' ,style:{width:"90%"},href:"htmlcss"},
+            { id: 1, text: 'javascript' ,style:{width:"90%"},href:"javascript"},
+            { id: 2, text: 'vue.js' ,style:{width:"90%"},href:"vue"},
+            { id: 3, text: 'webpack' ,style:{width:"90%"},href:"webpack"}
           ]
         }
       });
